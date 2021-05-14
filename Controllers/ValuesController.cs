b4f581abdff1b7bdf6983436280cfd76148d5367
Sunil patel <chemsp@outlook.com>
2021-05-14 11:06:24 -0400

    Add project files.

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIWithAuth.Models;
+
+namespace WebAPIWithAuth.Controllers
+{
+  // [Authorize(Roles ="Member, Admin")]
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            db_nameEntities1 _cntx = new db_nameEntities1();
+            var item = _cntx.QUOTES.ToList();
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+       [Route("myown")]      // http:localhost:4545/myown
+       [HttpGet]
+
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [Route("custom")]
+        public void Custom([FromBody]string value)
+        {
+        }
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
